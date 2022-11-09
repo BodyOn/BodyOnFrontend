47ba86004d66b2100addca8fccc8f4eb3df9311c
isaniovitor <isaniovitot@gmail.com>
2022-11-08 22:20:27 -0300

    nav between login and home

---
+++ src/main/java/com/example/bodyonfront/HomeController.java
@@ -0,0 +1,4 @@
+package com.example.bodyonfront;
+
+public class HomeController {
+}
--- src/main/java/com/example/bodyonfront/LoginController.java
+++ src/main/java/com/example/bodyonfront/LoginController.java
@@ -2,12 +2,17 @@ package com.example.bodyonfront;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,16 +25,15 @@ public class LoginController {
     public PasswordField passwordTextField;
 
     @FXML
-    protected void onLoginButtonClick(ActionEvent actionEvent) {
+    protected void onLoginButtonClick(ActionEvent actionEvent) throws IOException {
         if(!usernameTextField.getText().isBlank() && !passwordTextField.getText().isBlank()){
-//            LoginMessageLabel.setText("Houve uma tentativa de Logar");
-            validateLogin();
+            validateLogin(actionEvent);
         } else {
             System.out.println("Por favor digite usuário e senha");
         }
     }
 
-    private void validateLogin() {
+    private void validateLogin(ActionEvent event) {
         DatabaseConnection databaseConnection = new DatabaseConnection();
         Connection connection = databaseConnection.getConnection();
 
@@ -41,14 +45,23 @@ public class LoginController {
             ResultSet queryResult = statement.executeQuery(verifyLogin);
 
             while (queryResult.next()){
-                if (queryResult.getInt(1)==1){
-                    usernameTextField.setText("Bem Vindo!");
+                if (queryResult.getInt(1) == 1){
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
+                    Parent root = fxmlLoader.load();
+
+                    Stage stage = new Stage();
+                    stage.setTitle("BodyOn");
+                    stage.setResizable(false);
+                    stage.setScene(new Scene(root));
+                    stage.show();
                 }else{
                     usernameTextField.setText("Falha na autenticação");
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
     }
