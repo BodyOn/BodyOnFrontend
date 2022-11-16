0c158b218ce52d8ec5c748297f2d6be923b17aa1
josue.costa <jejosu346@gmail.com>
2022-11-16 01:00:36 -0300

    Add jdbc dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,10 @@ module com.example.bodyonfront {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires junit;
 
 
     opens com.example.bodyonfront to javafx.fxml;
     exports com.example.bodyonfront;
+    exports test.model.dao;
 }
