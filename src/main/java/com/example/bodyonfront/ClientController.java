b3f4e1d6ef48ba9772a23bfe76b2d93985104943
isaniovitor <isaniovitot@gmail.com>
2022-11-27 14:23:38 -0300

    pendency logic

--- src/main/java/com/example/bodyonfront/ClientController.java
+++ src/main/java/com/example/bodyonfront/ClientController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
@@ -50,16 +51,11 @@ public class ClientController implements Initializable {
     public Button registerClientButton;
     private final List<Cliente> clientList = new ArrayList<>();
 
+    private boolean pendencyButtonClicked = false;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        mockData();
-
-        // table listener
-        clientTable.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> onTableItemClick(newValue));
-    }
-
-    public void mockData() {
+        // iniciando colunas
         tableColumnClientAddressNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
         tableColumnClientAddress.setCellValueFactory(new PropertyValueFactory<>("endereco"));
         tableColumnClientFone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
@@ -72,12 +68,32 @@ public class ClientController implements Initializable {
         tableColumnClientWeight.setCellValueFactory(new PropertyValueFactory<>("peso"));
         tableColumnClientHeight.setCellValueFactory(new PropertyValueFactory<>("altura"));
 
+        mockData();
+
+        // table listener
+        clientTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> onTableItemClick(newValue));
+
+        clientTable.setRowFactory(tv -> new TableRow<Cliente>() {
+            @Override
+            protected void updateItem(Cliente item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item != null && item.isPendente()) {
+                    setStyle("-fx-background-color: #ffd7d1;");
+                } else {
+                    setStyle("");
+                }
+            }
+        });
+    }
+
+    public void mockData() {
         Cliente client = new Cliente(0, "isa", "rua tal", "8992828281", "isa@.com",
-                "922891910", 80.0, 1.80, "9h - 8h", 1, null, false);
+                "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
         Cliente client1 = new Cliente(1, "fals", "rua tal", "8992828281", "fals@.com",
                 "922891910", 80.0, 1.80, "9h - 8h", 1, null, false);
         Cliente client2 = new Cliente(2, "josus", "rua tal", "8992828281", "josus@.com",
-                "922891910", 80.0, 1.80, "9h - 8h", 1, null, false);
+                "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
 
         clientList.add(client);
         clientList.add(client1);
@@ -99,6 +115,43 @@ public class ClientController implements Initializable {
         stage.setResizable(false);
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void onPendencyButtonClick() {
+        if (pendencyButtonClicked) {
+            // pegar todos os clientes
+            clientList.clear();
+
+            Cliente client = new Cliente(0, "isa", "rua tal", "8992828281", "isa@.com",
+                    "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
+            Cliente client1 = new Cliente(1, "fals", "rua tal", "8992828281", "fals@.com",
+                    "922891910", 80.0, 1.80, "9h - 8h", 1, null, false);
+            Cliente client2 = new Cliente(2, "josus", "rua tal", "8992828281", "josus@.com",
+                    "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
+
+            clientList.add(client);
+            clientList.add(client1);
+            clientList.add(client2);
+
+            ObservableList<Cliente> observableList = FXCollections.observableArrayList(clientList);
+            clientTable.setItems(observableList);
+            pendencyButtonClicked = false;
+        }else{
+            // pegar todos os clientes pendentes
+            clientList.clear();
+
+            Cliente client = new Cliente(0, "isa", "rua tal", "8992828281", "isa@.com",
+                    "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
+            Cliente client2 = new Cliente(2, "josus", "rua tal", "8992828281", "josus@.com",
+                    "922891910", 80.0, 1.80, "9h - 8h", 1, null, true);
+
+            clientList.add(client);
+            clientList.add(client2);
+
+            ObservableList<Cliente> observableList = FXCollections.observableArrayList(clientList);
+            clientTable.setItems(observableList);
+            pendencyButtonClicked = true;
+        }
     }
 
     public void onTableItemClick(Cliente client)  {
